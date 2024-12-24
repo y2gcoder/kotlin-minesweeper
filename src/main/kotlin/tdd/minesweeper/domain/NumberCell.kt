785ab435bb6cba21586d111571e8e9b51634a23f
y2gcoder <y2gcoder@gmail.com>
2024-12-24 23:28:46 +0900

    feature(minesweeper): 숫자 셀은 열면 숫자 셀이다

--- src/main/kotlin/tdd/minesweeper/domain/NumberCell.kt
+++ src/main/kotlin/tdd/minesweeper/domain/NumberCell.kt
@@ -2,4 +2,6 @@ package tdd.minesweeper.domain
 
 class NumberCell : Cell {
     override fun isOpen(): Boolean = true
+
+    override fun open(): Cell = this
 }
--- src/test/kotlin/tdd/minesweeper/domain/NumberCellTest.kt
+++ src/test/kotlin/tdd/minesweeper/domain/NumberCellTest.kt
@@ -2,6 +2,7 @@ package tdd.minesweeper.domain
 
 import io.kotest.core.spec.style.BehaviorSpec
 import io.kotest.matchers.booleans.shouldBeTrue
+import io.kotest.matchers.types.shouldBeInstanceOf
 
 class NumberCellTest : BehaviorSpec({
     given("숫자 셀은") {
@@ -12,6 +13,14 @@ class NumberCellTest : BehaviorSpec({
 
             then("맞다") {
                 result.shouldBeTrue()
+            }
+        }
+
+        `when`("열면") {
+            val result = sut.open()
+
+            then("숫자 셀이다") {
+                result.shouldBeInstanceOf<NumberCell>()
             }
         }
     }
