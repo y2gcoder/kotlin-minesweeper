7e9571719076d75cffae3558d482278af3cfdc6a
y2gcoder <y2gcoder@gmail.com>
2024-12-24 23:54:11 +0900

    feature(minesweeper): 지뢰는 인접 지뢰 수를 0 ~ 8까지 가질 수 있다

--- src/main/kotlin/tdd/minesweeper/domain/NumberCell.kt
+++ src/main/kotlin/tdd/minesweeper/domain/NumberCell.kt
@@ -1,6 +1,10 @@
 package tdd.minesweeper.domain
 
-class NumberCell : Cell {
+data class NumberCell(val adjacentLandmines: Int = 0) : Cell {
+    init {
+        require(adjacentLandmines in (0..8)) { "인접 지뢰 수는 0 ~ 8까지만 가능하다: $adjacentLandmines" }
+    }
+
     override fun isOpen(): Boolean = true
 
     override fun open(): Cell = this
--- src/test/kotlin/tdd/minesweeper/domain/NumberCellTest.kt
+++ src/test/kotlin/tdd/minesweeper/domain/NumberCellTest.kt
@@ -1,7 +1,9 @@
 package tdd.minesweeper.domain
 
+import io.kotest.assertions.throwables.shouldThrow
 import io.kotest.core.spec.style.BehaviorSpec
 import io.kotest.matchers.booleans.shouldBeTrue
+import io.kotest.matchers.shouldBe
 import io.kotest.matchers.types.shouldBeInstanceOf
 
 class NumberCellTest : BehaviorSpec({
@@ -21,6 +23,20 @@ class NumberCellTest : BehaviorSpec({
 
             then("숫자 셀이다") {
                 result.shouldBeInstanceOf<NumberCell>()
+            }
+        }
+    }
+
+    given("인접 지뢰 수는") {
+        `when`("0 ~ 8 까지만") {
+            then("가능하다") {
+                listOf(-1, 9).forEach { adjacentLandmines ->
+                    shouldThrow<IllegalArgumentException> { NumberCell(adjacentLandmines) }
+                }
+                (0..8).forEach { adjacentLandmines ->
+                    val sut = NumberCell(adjacentLandmines)
+                    sut.adjacentLandmines shouldBe adjacentLandmines
+                }
             }
         }
     }
