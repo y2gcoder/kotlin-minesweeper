7d0f692af0c99f9161833d1cafbc47739ab663e3
y2gcoder <y2gcoder@gmail.com>
2024-12-27 01:16:59 +0900

    feature(minesweeper): 보드는 보드 내에 없는 위치의 셀을 열려고 하면 예외를 던짐

--- src/main/kotlin/tdd/minesweeper/domain/Board.kt
+++ src/main/kotlin/tdd/minesweeper/domain/Board.kt
@@ -1,3 +1,25 @@
 package tdd.minesweeper.domain
 
-data class Board(val area: Area, val cells: Cells)
+data class Board(val area: Area, val cells: Cells) {
+    fun open(location: Location): Board {
+        validateLocation(location)
+
+        return this.copy()
+    }
+
+    private fun validateLocation(location: Location) {
+        require(location in allLocations()) {
+            "보드 내의 위치가 아닙니다: location=$location"
+        }
+    }
+
+    private fun allLocations(): List<Location> {
+        return (0 until area.height * area.width)
+            .map {
+                Location(
+                    row = (it / area.width) + 1,
+                    col = (it % area.width) + 1,
+                )
+            }
+    }
+}
--- src/test/kotlin/tdd/minesweeper/domain/BoardTest.kt
+++ src/test/kotlin/tdd/minesweeper/domain/BoardTest.kt
@@ -1,7 +1,9 @@
 package tdd.minesweeper.domain
 
+import io.kotest.assertions.throwables.shouldThrow
 import io.kotest.core.spec.style.BehaviorSpec
 import io.kotest.matchers.shouldBe
+import tdd.minesweeper.domain.dsl.board
 
 class BoardTest : BehaviorSpec({
     given("영역과 셀 목록을 받아") {
@@ -14,6 +16,28 @@ class BoardTest : BehaviorSpec({
             then("생성할 수 있다") {
                 sut.cells shouldBe cells
                 sut.area shouldBe area
+            }
+        }
+    }
+
+    given("5 x 5 크기, 지뢰 개수 5개의 닫힌 셀만 존재하는 보드에서") {
+        val sut =
+            board {
+                height(5)
+                width(5)
+                countOfMines(5)
+                mineAt(1, 4)
+                mineAt(1, 5)
+                mineAt(2, 1)
+                mineAt(4, 3)
+                mineAt(5, 1)
+            }
+
+        `when`("보드 내에 없는 위치의 셀을 열려고 하면") {
+            val location = Location(0, 0)
+
+            then("예외를 던진다") {
+                shouldThrow<IllegalArgumentException> { sut.open(location) }
             }
         }
     }
