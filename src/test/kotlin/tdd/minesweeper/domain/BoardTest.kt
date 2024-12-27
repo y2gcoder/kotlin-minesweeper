1c667cf79ff9557968ac6762dffa1995a75f5404
y2gcoder <y2gcoder@gmail.com>
2024-12-27 11:10:35 +0900

    test(minesweeper): 보드의 이미 열린 셀을 열면 보드의 변화가 없음

--- src/test/kotlin/tdd/minesweeper/domain/BoardTest.kt
+++ src/test/kotlin/tdd/minesweeper/domain/BoardTest.kt
@@ -33,11 +33,35 @@ class BoardTest : BehaviorSpec({
                 mineAt(5, 1)
             }
 
-        `when`("보드 내에 없는 위치의 셀을 열려고 하면") {
+        `when`("보드 내에 없는 위치의 셀을 열면") {
             val location = Location(0, 0)
 
             then("예외를 던진다") {
                 shouldThrow<IllegalArgumentException> { sut.open(location) }
+            }
+        }
+    }
+
+    given("5 x 5 크기, 지뢰 개수 5개, 열린 셀 1개의 보드에서") {
+        val sut =
+            board {
+                height(5)
+                width(5)
+                countOfMines(5)
+                mineAt(1, 4)
+                mineAt(1, 5)
+                mineAt(2, 1)
+                mineAt(4, 3)
+                mineAt(5, 1)
+                openAt(1, 1)
+            }
+
+        `when`("이미 열린 셀을 다시 열면") {
+            val location = Location(1, 1)
+            val result = sut.open(location)
+
+            then("보드는 변하지 않는다") {
+                result shouldBe sut
             }
         }
     }
