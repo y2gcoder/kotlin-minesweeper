20155884f59aeaa30efaebf99d6b7afce0bea98b
y2gcoder <y2gcoder@gmail.com>
2024-12-29 00:57:21 +0900

    refactor(minesweeper): Board 의 open 리팩터링
    - open() 메서드 간소화: findAllShouldOpen()으로 책임 이전
    - findAllShouldOpen(): BFS를 통해 열어야 할 Set<Location> 계산
    - applyOpen(): Set<Location>을 인덱스 기반 Set 으로 변경 && 가변 리스트로 변경해서 계산 후 불변 리스트로 반환함으로써 성능 향상

--- src/main/kotlin/tdd/minesweeper/domain/Board.kt
+++ src/main/kotlin/tdd/minesweeper/domain/Board.kt
@@ -4,27 +4,62 @@ data class Board(val area: Area, val cells: Cells) {
     fun open(location: Location): Board {
         validateLocation(location)
 
-        val locationIndex = location.toIndex(area.width)
+        val shouldOpen = findAllShouldOpen(location)
 
-        val openedCells = cells.toMutableList()
-        openedCells[locationIndex] = openedCells[locationIndex].open()
-
-        return this.copy(cells = Cells(openedCells.toList()))
+        return this.copy(cells = applyOpen(shouldOpen))
     }
 
     private fun validateLocation(location: Location) {
-        require(location in allLocations()) {
+        require(location.isValid(area)) {
             "보드 내의 위치가 아닙니다: location=$location"
         }
     }
 
-    private fun allLocations(): List<Location> {
-        return (0 until area.height * area.width)
-            .map {
-                Location.from(
-                    index = it,
-                    width = area.width,
-                )
+    private fun findAllShouldOpen(location: Location): Set<Location> {
+        val result = mutableSetOf<Location>()
+        val queue = ArrayDeque<Location>().apply { add(location) }
+        val visited = mutableSetOf<Location>().apply { add(location) }
+
+        while (queue.isNotEmpty()) {
+            val current = queue.removeFirst()
+
+            // 현재 위치의 셀 상태 확인
+            val currentCell = cells[current.toIndex(area.width)]
+            // 이미 열린 셀
+            if (currentCell.isOpen()) {
+                continue
             }
+
+            // 셀 열기
+            result.add(current)
+
+            // 인접 지뢰 수가 0이면 인접 셀 추가 탐색
+            if (currentCell.isExpandableToAdjacent()) {
+                addExpandableCandidateToQueue(current, visited, queue)
+            }
+        }
+        return result.toSet()
+    }
+
+    private fun addExpandableCandidateToQueue(
+        current: Location,
+        visited: MutableSet<Location>,
+        queue: ArrayDeque<Location>,
+    ) {
+        AdjacentDirection.allAdjacentLocations(current)
+            .filter { it.isValid(area) && it !in visited }
+            .forEach { adjacent ->
+                queue.add(adjacent)
+                visited.add(current)
+            }
+    }
+
+    private fun applyOpen(shouldOpen: Set<Location>): Cells {
+        val shouldOpenIndexes = shouldOpen.map { it.toIndex(area.width) }.toSet()
+        val result = cells.toMutableList()
+
+        shouldOpenIndexes.forEach { index -> result[index] = cells[index].open() }
+
+        return Cells(result.toList())
     }
 }
