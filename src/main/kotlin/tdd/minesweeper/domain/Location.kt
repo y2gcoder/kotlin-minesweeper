f1d46778262abdb53e105df6a7f317a8d709bce1
y2gcoder <y2gcoder@gmail.com>
2024-12-28 23:42:57 +0900

    feature(minesweeper): 위치가 보드 영역 내에 있는 유효한 위치인지 체크할 수 있는 isValid(area: Area) 추가

--- src/main/kotlin/tdd/minesweeper/domain/Location.kt
+++ src/main/kotlin/tdd/minesweeper/domain/Location.kt
@@ -3,6 +3,10 @@ package tdd.minesweeper.domain
 data class Location(val row: Int, val col: Int) {
     fun toIndex(width: Int): Int = (row - 1) * width + (col - 1)
 
+    fun isValid(area: Area): Boolean {
+        return row in 1..area.height && col in 1..area.width
+    }
+
     companion object {
         fun from(
             index: Int,
--- src/test/kotlin/tdd/minesweeper/domain/LocationTest.kt
+++ src/test/kotlin/tdd/minesweeper/domain/LocationTest.kt
@@ -29,4 +29,48 @@ class LocationTest : BehaviorSpec({
             }
         }
     }
+
+    given("보드의 영역이 3x3일 때") {
+        val area = Area(height = 3, width = 3)
+
+        `when`("위치가 보드 내부에 있는 경우") {
+            val location = Location(row = 2, col = 2)
+
+            then("유효한 위치로 확인된다") {
+                location.isValid(area) shouldBe true
+            }
+        }
+
+        `when`("위치가 보드의 경계선 상에 있는 경우") {
+            val edgeLocations =
+                listOf(
+                    Location(row = 1, col = 1),
+                    Location(row = 1, col = 3),
+                    Location(row = 3, col = 1),
+                    Location(row = 3, col = 3),
+                )
+
+            then("모든 위치가 유효한 위치로 확인된다") {
+                edgeLocations.forEach { location ->
+                    location.isValid(area) shouldBe true
+                }
+            }
+        }
+
+        `when`("위치가 보드의 경계를 벗어난 경우") {
+            val invalidLocations =
+                listOf(
+                    Location(row = 0, col = 1),
+                    Location(row = 4, col = 1),
+                    Location(row = 1, col = 0),
+                    Location(row = 1, col = 4),
+                )
+
+            then("모든 위치가 유효하지 않은 위치로 확인된다") {
+                invalidLocations.forEach { location ->
+                    location.isValid(area) shouldBe false
+                }
+            }
+        }
+    }
 })
